2f799821b3a50e5a86bc611e1a1442fe93a8398c
Oleh Tryhuba <trigyba.oleg@gmail.com>
2019-02-23 16:04:29 +0200

    change menu and add mental

--- src/js/Mental.js
+++ src/js/Mental.js
@@ -3,68 +3,23 @@ const _ = require('lodash');
 
 export default class Mental {
 	
-	constructor(firstCountArr, lastCountArr, digit) {
+	constructor(firstCountArr, lastCountArr) {
 		this.firstCountArr = firstCountArr;
 		this.lastCountArr = lastCountArr;
-		this.digit = digit;
 	}
 	
-	getAbacusSimpleDouble(columns, rows) {
+	getMental() {
 		
-		const genSimple = (prev, j, sumRes) => {
-			const change = j % 2;
-			let count = (Math.random() * (this.lastCountArr - this.firstCountArr) + this.firstCountArr).toFixed(2),
-				smallCount = (Math.random() * (this.digit - this.firstCountArr) + this.firstCountArr).toFixed(2);
-			
-			let result = count;
-			
-			if (change) {
-				count = smallCount;
-			}
-			
-			console.log(`sss${sumRes} `);
-			
-			if (parseFloat(sumRes) >= parseFloat(count)) {
-				result = parseFloat(count) * -1;
-				console.log(`+| prev=${prev} count= ${count} res= ${result}`);
-			}
-			else {
-				result = parseFloat(count);
-				console.log(`-| prev=${prev} count= ${count} res= ${result}`);
-			}
-			
-			//проверка и присвоения первого числа в уравнении
-			if (!j) {
-				result = (Math.random() * (this.lastCountArr - this.firstCountArr) + this.firstCountArr).toFixed(2);
-				// console.log(`================ firstCount ==================== ${result}`);
-			}
-			
-			result = parseFloat(result).toFixed(2).toString().replace(".", ",");
-			return result;
-		};
 		
-		const result = [];
-		const sums = [];
-		for (let i = 0; i < columns; i++) {
-			result[i] = [];
-			const sumArr = [];
-			let sumRes = 0;
-			for (let j = 0; j < rows; j++) {
-				result[i][j] = genSimple(result[i][j - 1], j, sumRes);
-				sumRes += parseFloat(result[i][j]);
-			}
-			console.log(result[i]);
-// =========================================================================
-			result[i].forEach((item) => {
-				item = parseFloat(item.replace(",", "."));
-				sumArr.push(item);
-			});
-// =========================================================================
-			sums[i] = _.sum(sumArr).toFixed(2);
-			// console.log(`sums=${sums[i]} sum=${sum}`);
-		}
+		const proba = [9, 3, 5, 7, 1, 8, 2];
+		
+		const result = proba;
+		let sums = _.sum(result);
+		
+		
 		console.log(`======= result ====== `, result);
 		console.log(`======= sum ====== `, sums);
+		
 		return {
 			countsArr: result,
 			sumArr: sums
